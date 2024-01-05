4be2826b4431b9d927ffc21e2508d231702d0878
rayqu <rayqu.work@gmail.com>
2024-01-06 10:32:32 +1100

    fix: update sentry running env

--- src/main.ts
+++ src/main.ts
@@ -10,25 +10,27 @@ import * as Sentry from '@sentry/node';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
 
   // sentry.io setup
-  Sentry.init({
-    dsn: process.env.SENTRY_DSN,
-    tracesSampleRate: 1.0,
-    debug: process.env.NODE_ENV === 'production' ? false : true,
-    environment: process.env.NODE_ENV,
-    release: 'release_one',
-    integrations: [
-      new Sentry.Integrations.Http({ tracing: true }),
-      new Sentry.Integrations.GraphQL(),
-      new Sentry.Integrations.Apollo({ useNestjs: true }),
-    ],
-  });
-
-  // The request handler must be the first middleware on the app
-  app.use(Sentry.Handlers.requestHandler());
-  // TracingHandler creates a trace for every incoming request
-  app.use(Sentry.Handlers.tracingHandler());
+  if (configService.get('env') === 'production') {
+    Sentry.init({
+      dsn: process.env.SENTRY_DSN,
+      tracesSampleRate: 1.0,
+      debug: process.env.NODE_ENV === 'production' ? false : true,
+      environment: process.env.NODE_ENV,
+      release: 'release_one',
+      integrations: [
+        new Sentry.Integrations.Http({ tracing: true }),
+        new Sentry.Integrations.GraphQL(),
+        new Sentry.Integrations.Apollo({ useNestjs: true }),
+      ],
+    });
+    // The request handler must be the first middleware on the app
+    app.use(Sentry.Handlers.requestHandler());
+    // TracingHandler creates a trace for every incoming request
+    app.use(Sentry.Handlers.tracingHandler());
+  }
 
   // setup helmet
   app.use(
@@ -61,11 +63,12 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
 
   // setup GraphQL API file upload service
-  const configService = app.get(ConfigService);
   app.use('/graphql', graphqlUploadExpress(configService.get('uploader.middleware')));
 
-  // The error handler must be before any other error middleware and after all controllers
-  app.use(Sentry.Handlers.errorHandler());
+  if (configService.get('env') === 'production') {
+    // The error handler must be before any other error middleware and after all controllers
+    app.use(Sentry.Handlers.errorHandler());
+  }
 
   // setup global exception filter for Http requests
   const httpAdapter = app.get(HttpAdapterHost);
